45ae04b9863b44db1c9a49a41992d8800ac8264c
rubystar767 <rubystar767@gmail.com>
2016-04-10 03:02:12 -0400

    Role Model and association
    Set up the new model role and generated migration to associate with user
    model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410053608) do
+ActiveRecord::Schema.define(version: 20160410065547) do
 
   create_table "appointments", force: :cascade do |t|
     t.string   "pet_name"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 20160410053608) do
     t.datetime "updated_at",       null: false
   end
 
+  create_table "roles", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -63,9 +69,11 @@ ActiveRecord::Schema.define(version: 20160410053608) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["role_id"], name: "index_users_on_role_id"
 
 end
